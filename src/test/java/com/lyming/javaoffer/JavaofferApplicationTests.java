a00e4af6bd9b66d408886a848cec7d4b06b3aa5b
Nero_Li <375892799@qq.com>
2020-07-17 15:56:42 +0800

    Initial commit

---
+++ src/test/java/com/lyming/javaoffer/JavaofferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyming.javaoffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaofferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
